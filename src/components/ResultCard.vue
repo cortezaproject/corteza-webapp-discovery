b17b7f9f2ba6bbe972dbe8d673f9d88a06d1c204
Matija Rešek <matija.resek@gmail.com>
2021-10-27 13:28:48 +0200

    Make card fields dynamic

--- src/components/ResultCard.vue
+++ src/components/ResultCard.vue
@@ -1,31 +1,37 @@
 <template>
-  <div>
-    <b-card
-      :title="hit.value.name"
-      class="shadow-sm"
-    >
+  <b-card
+    no-body
+    class="shadow-sm h-100"
+  >
+    <b-card-body class="d-flex flex-column">
+      <h4>
+        {{ value.name }}
+      </h4>
       <b-card-text class="mb-2">
-        <span class="text-info font-weight-bold mr-3">Account Name:</span>Accounts Germany
+        <span class="text-info font-weight-bold mr-3">Type:</span>{{ typeName }}
       </b-card-text>
-      <b-card-text class="mb-2">
-        <span class="text-info font-weight-bold mr-3">Type:</span>{{ hit.type }}
-      </b-card-text>
-      <b-card-text class="mb-2">
-        <span class="text-info font-weight-bold mr-3">Industry:</span>Consulting
-      </b-card-text>
-      <b-card-text class="mb-2">
-        <span class="text-info font-weight-bold mr-3">Account Source:</span>Customer Referral
-      </b-card-text>
-
-      <b-button
-        href="#"
-        variant="info"
-        class="mt-2"
+      <b-card-text
+        v-if="type === 'compose:namespace' || 'compose:module'"
+        class="mb-2"
       >
-        Show more
-      </b-button>
-    </b-card>
-  </div>
+        <span class="text-info font-weight-bold mr-3">Handle:</span>{{ value.handle }}
+      </b-card-text>
+      <b-card-text
+        v-if="type === 'compose:module'"
+        class="mb-2"
+      >
+        <span class="text-info font-weight-bold mr-3">Namespace:</span>{{ value.namespace.name }}
+      </b-card-text>
+      <div class="mt-auto">
+        <b-button
+          href="#"
+          variant="info"
+        >
+          Show more
+        </b-button>
+      </div>
+    </b-card-body>
+  </b-card>
 </template>
 
 <script>
@@ -35,6 +41,22 @@ export default {
       type: Object,
       default: null,
     },
+  },
+  data () {
+    return {
+      type: null,
+      value: null,
+    }
+  },
+  computed: {
+    typeName () {
+      if (this.type === 'compose:namespace') return 'Namespace'
+      return 'Module'
+    },
+  },
+  created () {
+    if (this.hit.type) this.type = this.hit.type
+    if (this.hit.value) this.value = this.hit.value
   },
 }
 </script>
