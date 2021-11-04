6a431636fbbf37cd98c48d484c3a7a990ad82cf0
Matija Rešek <matija.resek@gmail.com>
2021-11-04 10:22:03 +0100

    Design record card

--- src/components/ResultCard.vue
+++ src/components/ResultCard.vue
@@ -5,9 +5,12 @@
   >
     <b-card-body class="d-flex flex-column">
       <h4>
-        {{ hit.value.name }}
+        {{ title }}
       </h4>
-      <b-card-text class="mb-2">
+      <b-card-text
+        v-if="hit.type !== 'compose:record'"
+        class="mb-2"
+      >
         <span class="text-info font-weight-bold mr-3">{{ this.$t('card.type') }}:</span>{{ typeName }}
       </b-card-text>
       <b-card-text
@@ -33,6 +36,11 @@
         class="mb-2"
       >
         <span class="text-info font-weight-bold mr-3">{{ this.$t('card.module') }}:</span>{{ hit.value.module.name }}
+      </b-card-text><b-card-text
+        v-if="hit.type === 'compose:record'"
+        class="mb-2"
+      >
+        <span class="text-info font-weight-bold mr-3">{{ this.$t('card.fields') }}:</span>{{ fields }}
       </b-card-text>
       <div class="mt-auto">
         <b-button
@@ -63,6 +71,19 @@ export default {
       if (this.hit.type === 'system:user') return this.$t('card.user')
       if (this.hit.type === 'compose:record') return this.$t('card.record')
       return this.$t('card.module')
+    },
+    title () {
+      return this.hit.value.name ? this.hit.value.name : this.$t('card.record')
+    },
+    fields () {
+      let arr = []
+      Object.keys(this.hit.value.values).forEach((prop) => arr.push(prop))
+      if (arr.length > 10) {
+        arr = arr.slice(0, 8).join(', ') + ',...'
+      } else {
+        arr = arr.join(', ')
+      }
+      return arr
     },
   },
 }
